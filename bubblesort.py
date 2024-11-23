5ed2057dc3f8468d860ac4de58767a44d7d81d0b
Ashish Jha <ajlti2019@gmail.com>
2024-11-23 12:24:00 +0530

    added bubblesort in python

---
+++ bubblesort.py
@@ -0,0 +1,13 @@
+#implementing bubblesort in python
+
+def bubble_sort(input):
+    for i in range(len(input) - 1):
+        for j in range(0, len(input)-1):
+            if input[j] > input[j+1]:
+                input[j], input[j+1] = input[j+1], input[j]
+
+
+input = [5,9,3,5,15, 7, 11,2]
+
+print(bubble_sort(input))
+
