bac573e4deb42639d4ddab9eecf1b9d94ac52268
Ashish Jha <ajlti2019@gmail.com>
2024-11-26 06:16:18 +0530

    updated quick sort

---
+++ quicksort.py
@@ -0,0 +1,11 @@
+#implementation of quicksort in python
+
+def quick_sort(input):
+    for i in range(len(input)):
+        pivot = input[0]
+
+    pass
+
+
+input = [5,0, 8,7,9, 3, 6, 11, 99,4]
+print(quick_sort(input))
